82198f8332d0ba128dbba47586441ada05abe3cf
m4nWiLLBr00t <manoharwilly33@gmail.com>
2025-08-05 19:31:09 +0530

    Created pattern1.cpp

---
+++ AlphabeticPatterns/pattern1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+  int num;
+  char alphabet = 'A';
+  std::cout << "Enter a number between(1-26): ";
+  std::cin >> num;
+
+  if (num >= 1 && num <= 26)
+  {
+    for (int rows = 1; rows <= num; rows++)
+    {
+      for (int cols = 1; cols <= num; cols++)
+      {
+        std::cout << char(alphabet + rows - 1) << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+  else
+  {
+    std::cout << "Enter a number between(1-26) only";
+  }
+  return 0;
+}
